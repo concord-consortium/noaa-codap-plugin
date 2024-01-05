76a8f0b2005040d6ccc7150851d28ba46ab4ff34
Doug Martin <dmartin@concord.org>
2024-01-05 13:19:17 -0500

    feat: Added state managment using immer with a context provider [PT-186786921]

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+// any new types should be created here...
+
+export interface IState {
+  counterToTestStateChanges: number;
+}
+
+export const DefaultState: IState = {
+  counterToTestStateChanges: 0,
+};
