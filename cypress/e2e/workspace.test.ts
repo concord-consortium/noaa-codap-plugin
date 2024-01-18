717ea12451353be1c43bf091fc0d73cd85c46186
lublagg <lvblagg@gmail.com>
2024-01-18 15:51:15 -0500

    Remove unused e2e test.

--- cypress/e2e/workspace.test.ts
+++ cypress/e2e/workspace.test.ts
@@ -1,13 +1,5 @@
-import { AppElements as ae } from "../support/elements/app-elements";
-
 context("Test the overall app", () => {
   beforeEach(() => {
     cy.visit("");
-  });
-
-  describe("Desktop functionalities", () => {
-    it("renders with text", () => {
-      ae.getApp().should("have.text", "Hello World");
-    });
   });
 });
